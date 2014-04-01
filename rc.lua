86f4afa90b5f3d1451a04a559010051d2a65cc6b
Joaquim Pedro França Simão (osmano807) <osmano807@gmail.com>
2014-04-01 12:30:51 -0300

    Fix key lua error

--- rc.lua
+++ rc.lua
@@ -436,7 +436,7 @@ globalkeys = awful.util.table.join(
     awful.key({ modkey, "Control" }, "l",     function () awful.tag.incncol(-1)         end),
     awful.key({ modkey,           }, "space", function () awful.layout.inc(layouts,  1) end),
     awful.key({ modkey, "Shift"   }, "space", function () awful.layout.inc(layouts, -1) end),
-    awful.key({ modkey,           }, "w",     function () awful.util.spawn("luakit")    end, "Start Luakit Web Browser"),
+    awful.key({ modkey,           }, "w",     function () awful.util.spawn("chromium")  end),
 
     awful.key({ modkey, "Control" }, "n", awful.client.restore),
 
